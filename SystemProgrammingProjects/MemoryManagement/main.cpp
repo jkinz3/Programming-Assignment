8ae09a1ca49538c072603043b993374626daba85
johnkinzel <john.kinzel426@gmail.com>
2015-12-11 11:00:24 -0600

    Wrote main algorithm
    It doesn't fill each row with the previous contents.

--- SystemProgrammingProjects/MemoryManagement/main.cpp
+++ SystemProgrammingProjects/MemoryManagement/main.cpp
@@ -39,7 +39,7 @@ void PrintFrames(int frames[][4])
 }
 
 
-int SearchFrames(int frames[][4], int x, int replace)
+int SearchFrames(int frames[][4], int x, int replace, int first)
 {
 	
 	for (int i = 0; i < 4; i++)
@@ -48,19 +48,16 @@ int SearchFrames(int frames[][4], int x, int replace)
 		{
 			return -1;
 			break;
+			
 		}
-		if (frames[x][y] == -1)
+		if (frames[x][i] == -1)
 		{
 			return i;
 			break;
 		}
-		else
-		{
-
-		}
-
+		
 	}
-
+	return first;
 
 
 	
@@ -84,10 +81,12 @@ int main()
 
 	int y; //the Y of each row to swap
 
+	queue<int> fillFrames; //this queue keeps track of the numbers that were entered so we can fill up the frames that were written before.
+
 	queue<int> firstReplace; //keeps track of which rows were replaced
 	firstReplace.push(0); //first replacement will be 0 by default so this will get the algorithm started.
 
-	int Frames[10][4];//the frames to be replaced
+	int Frames[ARRAY_SIZEX][ARRAY_SIZEY];//the frames to be replaced
 
 	for (int i = 0; i < ARRAY_SIZEX; i++)
 	{
@@ -99,13 +98,14 @@ int main()
 
 
 
+	cout << "Hello! We are going to do some FIFO! Remember that -1 means EMPTY\n\n";
+
+	PrintFrames(Frames);
 
 
 	do{//repeat until 10 numbers are used
 
-		cout << "Hello! We are going to do some FIFO!\n\n";
-
-		PrintFrames(Frames);
+		
 
 		
 
@@ -125,7 +125,7 @@ int main()
 //We have the input. Now it's time to search through the frames to decide which number to replace
 		
 		
-			y = SearchFrames(Frames, count, input);//call function to search for  which row to swap
+			y = SearchFrames(Frames, count, input, firstReplace.front());//call function to search for  which row to swap
 
 			
 
@@ -134,25 +134,31 @@ int main()
 			case -1:
 				cout << "The number already exists in this row. No entry needed\n\n";
 				PrintFrames(Frames);
+				count++;
 				break;
 			case -2:
 				cout << "Empty space found. No replacement needed\n\n";
-				firstReplace.push(y);
+				Frames[count][y] = input;
+				firstReplace.push(y);//push y onto queue
+				firstReplace.pop();//remove the first
 				PrintFrames(Frames);
+				count++;
 				break;
 			default:
 				Frames[count][y] = input;
 				cout << "No space found. Replacing the frame that was entered first\n\n";
-				firstReplace.push(y);
+				firstReplace.push(y);//push y onto queue
+				firstReplace.pop();//remove the first.
 				PrintFrames(Frames);
+				count++;
 				break;
-
-
 			}
 		
-		
-
-	}while (again == 0);
+			if (count == 10)
+			{
+				cout << "\n\n\nYou are done! Thank you!\n\n";
+			}
+	}while (count < 11);
 	
 
 
